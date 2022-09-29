2fa025c9693c40cd33497ef029bd6bc1bb776336
Andrej Petras <andrej@lorislab.org>
2022-09-29 11:00:29 +0200

    fix: cdi log start event (#18)

---
+++ extensions/log/cdi/deployment/src/test/java/org/tkit/quarkus/log/cdi/test/AppLifecycleServiceTest.java
@@ -0,0 +1,27 @@
+package org.tkit.quarkus.log.cdi.test;
+
+import javax.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+import org.tkit.quarkus.log.cdi.test.app.AppLifecycleService;
+
+import io.quarkus.test.QuarkusUnitTest;
+
+public class AppLifecycleServiceTest {
+
+    @RegisterExtension
+    static final QuarkusUnitTest config = new QuarkusUnitTest()
+            .withApplicationRoot((jar) -> jar
+                    .addClasses(AppLifecycleService.class)
+                    .addAsResource("default.properties", "application.properties"));
+
+    @Inject
+    AppLifecycleService service;
+
+    @Test
+    public void startup() {
+        service.info();
+    }
+
+}
