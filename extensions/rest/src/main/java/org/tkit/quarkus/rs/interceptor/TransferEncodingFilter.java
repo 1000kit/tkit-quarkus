3114c985bf024e4794fa8b24a63942abd3146ffd
lukasz.laszkiewicz <lukasz.laszkiewicz@capgemini.com>
2023-08-25 14:09:09 +0200

    feat: global transfer encoding filter see: https://github.com/quarkusio/quarkus/issues/29059

--- extensions/rest/src/main/java/org/tkit/quarkus/rs/interceptor/TransferEncodingFilter.java
+++ extensions/rest/src/main/java/org/tkit/quarkus/rs/interceptor/TransferEncodingFilter.java
@@ -3,11 +3,18 @@ package org.tkit.quarkus.rs.interceptor;
 import javax.ws.rs.client.ClientRequestContext;
 import javax.ws.rs.client.ClientResponseContext;
 import javax.ws.rs.client.ClientResponseFilter;
+import javax.ws.rs.ext.Provider;
 
+@Provider
 public class TransferEncodingFilter implements ClientResponseFilter {
+
+    public static final String TRANSFER_ENCODING = "Transfer-Encoding";
 
     @Override
     public void filter(ClientRequestContext clientRequestContext, ClientResponseContext clientResponseContext) {
-        clientResponseContext.getHeaders().remove("Transfer-Encoding");
+        String teHeaderValue = clientResponseContext.getHeaderString(TRANSFER_ENCODING);
+        if (teHeaderValue != null && teHeaderValue.contains("chunked")) {
+            clientResponseContext.getHeaders().remove(TRANSFER_ENCODING);
+        }
     }
 }
