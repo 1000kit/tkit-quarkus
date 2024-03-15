c70d9e8965713a5777850051d8ba045fd5d0de27
Andrej Petras <andrej@lorislab.org>
2024-03-15 15:39:31 +0100

    feat: add documentation

--- extensions/security/src/main/java/org/tkit/quarkus/security/SecurityConfig.java
+++ extensions/security/src/main/java/org/tkit/quarkus/security/SecurityConfig.java
@@ -1,12 +1,20 @@
 package org.tkit.quarkus.security;
 
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
+import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
+@StaticInitSafe
 @ConfigMapping(prefix = "tkit.security")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
 public interface SecurityConfig {
 
+    /**
+     * Disable or enabled authentication
+     */
     @WithName("auth.enabled")
     @WithDefault("true")
     boolean enabled();
