39f4e9899acc2f16fe6085987a845f21d5f61887
Andrej Petras <andrej@lorislab.org>
2024-05-06 10:45:09 +0200

    feat: revert hint from page query (#160)

--- extensions/jpa/tests/src/main/java/org/tkit/quarkus/jpa/test/ChildDAO.java
+++
@@ -1,9 +0,0 @@
-package org.tkit.quarkus.jpa.test;
-
-import jakarta.enterprise.context.ApplicationScoped;
-
-import org.tkit.quarkus.jpa.daos.AbstractDAO;
-
-@ApplicationScoped
-public class ChildDAO extends AbstractDAO<Child> {
-}
