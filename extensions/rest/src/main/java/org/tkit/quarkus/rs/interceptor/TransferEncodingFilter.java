24a17ef1c081a7da702f4ccfa7bdeb47073883bb
Michal Somora <michal.somora@gmail.com>
2024-07-08 14:37:04 +0200

    fix: @provider for TransferEncodingFilter

--- extensions/rest/src/main/java/org/tkit/quarkus/rs/interceptor/TransferEncodingFilter.java
+++ extensions/rest/src/main/java/org/tkit/quarkus/rs/interceptor/TransferEncodingFilter.java
@@ -3,7 +3,9 @@ package org.tkit.quarkus.rs.interceptor;
 import jakarta.ws.rs.client.ClientRequestContext;
 import jakarta.ws.rs.client.ClientResponseContext;
 import jakarta.ws.rs.client.ClientResponseFilter;
+import jakarta.ws.rs.ext.Provider;
 
+@Provider
 public class TransferEncodingFilter implements ClientResponseFilter {
 
     @Override
