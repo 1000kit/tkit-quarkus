056194c1a2a8f6b069b822586092ea24de6aa9cb
Andrej Petras <andrej@lorislab.org>
2025-01-24 13:55:48 +0100

    feat: update metrics test

--- it/security-test/src/test/java/org/tkit/quarkus/it/security/test/test/SecurityDynamicTestIT.java
+++ it/security-test/src/test/java/org/tkit/quarkus/it/security/test/test/SecurityDynamicTestIT.java
@@ -1,7 +1,8 @@
 package org.tkit.quarkus.it.security.test.test;
 
-import io.quarkus.test.junit.QuarkusIntegrationTest;
 import org.tkit.quarkus.security.test.openapi.SecurityDynamicTest;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
 
 @QuarkusIntegrationTest
 public class SecurityDynamicTestIT extends SecurityDynamicTest {
