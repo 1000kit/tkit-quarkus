ee92afecdd6121167f5734f5a71013856c9c53d9
Andrej Petras <andrej@lorislab.org>
2025-05-08 17:27:26 +0200

    tests: add agroal metrics test

--- it/metrics-test/src/test/java/org/tkit/quarkus/it/metrics/test/PingMetricsTest.java
+++ it/metrics-test/src/test/java/org/tkit/quarkus/it/metrics/test/PingMetricsTest.java
@@ -2,6 +2,8 @@ package org.tkit.quarkus.it.metrics.test;
 
 import static io.restassured.RestAssured.given;
 
+import jakarta.ws.rs.core.Response;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import io.quarkus.test.junit.QuarkusTest;
@@ -10,10 +12,16 @@ import io.quarkus.test.junit.QuarkusTest;
 public class PingMetricsTest {
     @Test
     void pingMetrics() {
-        given()
+        var response = given()
                 .when()
                 .get("/q/metrics")
                 .then()
-                .statusCode(200);
+                .statusCode(Response.Status.OK.getStatusCode())
+                .extract().asString();
+
+        // check extended agroal metrics
+        Assertions.assertNotNull(response);
+        Assertions.assertTrue(response.contains("agroal_config_pool_max_size_total{datasource=\"default\"}"));
+        Assertions.assertTrue(response.contains("agroal_config_pool_min_size_total{datasource=\"default\"}"));
     }
 }
