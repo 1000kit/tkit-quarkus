b64476f12fc411f33f792f807bc188a049e22897
Andrej Petras <andrej@lorislab.org>
2025-07-25 10:00:05 +0200

    doc: update tkit-rest documentation (#321)

--- extensions/rest/src/main/java/org/tkit/quarkus/rs/RestConfig.java
+++ extensions/rest/src/main/java/org/tkit/quarkus/rs/RestConfig.java
@@ -2,13 +2,18 @@ package org.tkit.quarkus.rs;
 
 import java.util.List;
 
+import io.quarkus.runtime.annotations.ConfigDocFilename;
+import io.quarkus.runtime.annotations.ConfigPhase;
+import io.quarkus.runtime.annotations.ConfigRoot;
 import io.quarkus.runtime.annotations.StaticInitSafe;
 import io.smallrye.config.ConfigMapping;
 import io.smallrye.config.WithDefault;
 import io.smallrye.config.WithName;
 
 @StaticInitSafe
+@ConfigDocFilename("tkit-rest.adoc")
 @ConfigMapping(prefix = "tkit.rs")
+@ConfigRoot(phase = ConfigPhase.RUN_TIME)
 public interface RestConfig {
 
     /**
