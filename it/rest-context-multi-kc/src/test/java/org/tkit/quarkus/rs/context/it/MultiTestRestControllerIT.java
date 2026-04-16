605fa63aa2a12fb70b2a99d4e121b9e0239c0800
Andrej Petras <andrej@lorislab.org>
2026-04-16 18:49:48 +0200

    fix: fix token parser for native image (#403)
    (cherry picked from commit 3ce1e528eff23753f2fe213436d0e62e4202e76e)

---
+++ it/rest-context-multi-kc/src/test/java/org/tkit/quarkus/rs/context/it/MultiTestRestControllerIT.java
@@ -0,0 +1,8 @@
+package org.tkit.quarkus.rs.context.it;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class MultiTestRestControllerIT extends MultiTestRestControllerTest {
+
+}
